326f62a419857262eae750e969b7003b99116cac
Eoin-k <100050380+Eoin-k@users.noreply.github.com01~>
2024-09-13 22:12:05 +0100

    Added Validation for signup and post creation

--- Controllers/messageController.js
+++ Controllers/messageController.js
@@ -1,4 +1,19 @@
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const validateMessage = [
+	body("title")
+		.trim()
+		.escape()
+		.isLength({ min: 3 })
+		.withMessage("Title must be longer than 3 characters"),
+	body("post_content")
+		.trim()
+		.escape()
+		.isLength({ min: 5 })
+		.withMessage("Post content must be longer than 5 characters"),
+];
+
 showHome = async (req, res) => {
 	try {
 		const messages = await db.getAllMessages();
@@ -11,15 +26,27 @@ showHome = async (req, res) => {
 	}
 };
 
-createMessage = async (req, res) => {
-	const title = req.body.title;
-	const postContent = req.body.post_content;
-	const userId = req.user.user_id;
-	try {
-		await db.addNewMessage(title, postContent, userId);
-		res.redirect("/");
-	} catch (err) {}
-};
+createMessage = [
+	validateMessage,
+	async (req, res) => {
+		const title = req.body.title;
+		const postContent = req.body.post_content;
+		const userId = req.user.user_id;
+		const messages = await db.getAllMessages();
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).render("index", {
+					user: req.user,
+					errors: errors.array(),
+					messages: messages,
+				});
+			}
+			await db.addNewMessage(title, postContent, userId);
+			res.redirect("/");
+		} catch (err) {}
+	},
+];
 
 deleteMessage = async (req, res) => {
 	try {
--- Controllers/signupController.js
+++ Controllers/signupController.js
@@ -1,5 +1,29 @@
 const passwordUtils = require("../Utilities/passwordUtils");
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const validateSignup = [
+	body("email")
+		.trim()
+		.isEmail()
+		.escape()
+		.withMessage("Please enter a valid email"),
+	body("firstname")
+		.trim()
+		.escape()
+		.isLength({ min: 2 })
+		.withMessage("First name must be longer than 2 characters"),
+	body("lastname")
+		.trim()
+		.escape()
+		.isLength({ min: 2 })
+		.withMessage("Last name must be longer than 2 characters"),
+	body("password")
+		.trim()
+		.escape()
+		.isLength({ min: 3 })
+		.withMessage("Password must be longer than 3 characters"),
+];
 
 showRegistration = async (req, res) => {
 	try {
@@ -11,19 +35,30 @@ showRegistration = async (req, res) => {
 	}
 };
 
-registerUser = async (req, res) => {
-	const password = req.body.password;
-	const email = req.body.email;
-	const firstname = req.body.firstname;
-	const lastname = req.body.lastname;
-	try {
-		const hashedpassword = await passwordUtils.generatePassword(password);
-		await db.addNewUser(email, hashedpassword, firstname, lastname);
-		res.redirect("/");
-	} catch (err) {
-		console.error(err);
-	}
-};
+registerUser = [
+	validateSignup,
+	async (req, res) => {
+		try {
+			const errors = await validationResult(req);
+			console.log("shit");
+			if (!errors.isEmpty()) {
+				return res.status(400).render("signup", {
+					user: req.user,
+					errors: errors.array(),
+				});
+			}
+			const password = req.body.password;
+			const email = req.body.email;
+			const firstname = req.body.firstname;
+			const lastname = req.body.lastname;
+			const hashedpassword = await passwordUtils.generatePassword(password);
+			await db.addNewUser(email, hashedpassword, firstname, lastname);
+			res.redirect("/");
+		} catch (err) {
+			console.error(err);
+		}
+	},
+];
 
 module.exports = {
 	showRegistration,
